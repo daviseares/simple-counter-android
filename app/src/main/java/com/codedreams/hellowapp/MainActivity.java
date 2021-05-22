433678b0c25a6b42b78da16a8637140c7c8ef052
daviseares <davi.bseares@gmail.com>
2021-05-22 17:48:41 -0300

    init app

---
+++ app/src/main/java/com/codedreams/hellowapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.codedreams.hellowapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView text;
+
+    Integer x = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button  =  findViewById(R.id.btn_limpar);
+        text = findViewById(R.id.count);
+    }
+
+    /**
+     * format number into string
+     */
+    protected String strFormat(Integer i){
+        return String.format(Locale.getDefault(), "%d", i);
+    }
+
+    /**
+     * increment counter;
+     */
+    public void handleIncrement(View view) {
+        x+=1;
+        text.setText(strFormat(x));
+    }
+
+    /**
+     * decrement counter;
+     */
+    public void handleDecrement(View view) {
+        x-=1;
+        text.setText(strFormat(x));
+    }
+
+    /**
+     * reset counter;
+     */
+    public void handleClean(View view) {
+        x = 0;
+        text.setText(strFormat(x));
+    }
+
+
+}
